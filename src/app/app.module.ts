d7e9c1d593e1b593ecc171fa0b359ae0901fee84
AnandRaja-tech <anand.official.in@gmail.com>
2021-12-22 16:59:33 +0000

    test code only

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,7 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { MousePageModule } from './pages/mouse-page/mouse-page.module';
-import { MouseModule } from './pages/mouse-page/mouse/mouse.module';
+import { NavBarModule } from './pages/nav-bar/nav-bar.module';
 import { TogglePageModule } from './pages/toggle-page/toggle-page.module';
 import { SharedModule } from './shared/shared.module';
 
@@ -16,9 +16,9 @@ import { SharedModule } from './shared/shared.module';
     BrowserModule,
     AppRoutingModule,
     SharedModule,
-    MouseModule,
     MousePageModule,
-    TogglePageModule
+    TogglePageModule,
+    NavBarModule
   ],
   providers: [],
   bootstrap: [AppComponent]
